938b5b6dbdb07e50c23ff86bad96282973ca0982
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-10-07 10:22:03 -0500

    sets up saga and reducer for getting all the signed up volunteers for a specifc event

--- src/redux/sagas/volunteerSaga.js
+++ src/redux/sagas/volunteerSaga.js
@@ -11,9 +11,24 @@ function* getVolunteerRoles(action) {
   }
 }
 
+//requests all the volunteers that are signed up for this specific event
+function* getSpecifcVolunteers(action) {
+  try{
+    let response = yield axios.get(`/api/volunteer/eventVolunteers/${action.payload}`)
+    yield put({
+      type: 'SET_SPECIFIC_VOLUNTEERS',
+      payload: response.data
+    })
+  }
+  catch (error) {
+    console.log('error in getSpecificVolunteers')
+  }
+}
+
 //root saga
 function* volunteerSaga() {
   yield takeLatest('GET_EVENT_DETAILS', getVolunteerRoles);
+  yield takeLatest('GET_SPECIFIC_VOLUNTEERS', getSpecifcVolunteers);
 }
 
 export default volunteerSaga;
