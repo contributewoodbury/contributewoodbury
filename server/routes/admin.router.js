d732fe4f7b448a385597e955b59180b8d3f38059
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-10-07 11:32:56 -0500

    creates router for updating approval of nonprofit

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -3,5 +3,18 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
+//updates nonprofit's approval to true
+router.put('/approve/:id', (req, res) => {
+    let queryText = `UPDATE "nonprofit" SET "is_approved" = 'true' WHERE "id" = $1;`;
+    let id = req.params.id
+    pool.query(queryText, [id])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error in updating approval for nonprofit', error)
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
