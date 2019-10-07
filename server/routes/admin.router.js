eb96ed79b4dcbdaa7ade556eef32ef8eb62ddb28
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-10-07 13:52:58 -0500

    adds extra security to admin

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -5,6 +5,7 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 //updates nonprofit's approval to true
 router.put('/approve/:id', rejectUnauthenticated, (req, res) => {
+    if (req.user.name === 'Admin') {
     let queryText = `UPDATE "nonprofit" SET "is_approved" = 'true' WHERE "id" = $1;`;
     let id = req.params.id
     pool.query(queryText, [id])
@@ -15,10 +16,14 @@ router.put('/approve/:id', rejectUnauthenticated, (req, res) => {
             console.log('error in updating approval for nonprofit', error)
             res.sendStatus(500);
         })
+    } else {
+        sendStatus(403)
+    }
 })
 
 //deletes a nonprofit when their request is denied
 router.delete('/decline/:id', rejectUnauthenticated, (req,res) => {
+    if (req.user.name === 'Admin') {
     let queryText = `DELETE FROM "nonprofit" WHERE "id" = $1;`;
     let id = req.params.id;
     pool.query(queryText, [id])
@@ -29,10 +34,14 @@ router.delete('/decline/:id', rejectUnauthenticated, (req,res) => {
             console.log('error in deleted declined requests for nonprofits', error)
             res.sendStatus(500);
         })
+    } else {
+        sendStatus(403)
+    }
 })
 
 //gets the nonprofit requests from the database
 router.get('/requests', rejectUnauthenticated, (req, res) => { 
+    if (req.user.name === 'Admin') {
     let queryText = `SELECT * FROM "nonprofit" WHERE "is_approved" = 'false';`;
     pool.query(queryText)
         .then((result) => {
@@ -42,6 +51,9 @@ router.get('/requests', rejectUnauthenticated, (req, res) => {
             console.log('error in getting requested for nonprofits', error)
             res.sendStatus(500);
         })
+    } else {
+        sendStatus(403)
+    }
 })
 
 module.exports = router;
