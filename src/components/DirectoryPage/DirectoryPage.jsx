ece45b9749633f99531d6f33ffd2714a9f6e029f
Matt Lissick <mattlissick@gmail.com>
2019-10-08 14:34:36 -0500

    table and components are hardwired

--- src/components/DirectoryPage/DirectoryPage.jsx
+++ src/components/DirectoryPage/DirectoryPage.jsx
@@ -10,6 +10,9 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = theme => ({
     backButton: {
@@ -18,11 +21,21 @@ const styles = theme => ({
         margin: '0px 0px 0px 30px'
     },
     root: {
-        maxHeight: '300px',
-        width: '90%',
+        maxHeight: '800px',
+        width: '80%',
         marginTop: theme.spacing.unit * 3,
         overflowY: 'scroll',
+        margin: 'auto'
     },
+    search: {
+        float: 'right',
+        width: '50%',
+        margin: theme.spacing.unit * 3,
+        
+    },
+    rows: {
+        height: '100px'
+    }
 
 })
 
@@ -39,24 +52,37 @@ class DirectoryPage extends Component {
                     In Directory Page
                 </h1>
 
+                
+
                 <Paper className={this.props.classes.root}>
-                    <Table>
+
+                    <div className={this.props.classes.search}>
+                        <Paper elevation={1}>
+                            <IconButton aria-label="Search">
+                                <SearchIcon />
+                            </IconButton>
+                            <InputBase placeholder="Search Local Nonprofits" />
+                            <Divider/>
+                        </Paper>
+                    </div>
+
+                    <Table hover={true} size="large">
                         <TableHead>
-                            <TableRow>
-                                <TableCell align="right">Image</TableCell>
-                                <TableCell align="right">Agency</TableCell>
-                                <TableCell align="right">Category</TableCell>
-                                <TableCell align="right">Volunteer Opportunities</TableCell>
-                                <TableCell align="right">Website Link</TableCell>
+                            <TableRow className={this.props.classes.rows}>
+                                <TableCell align="center">Image</TableCell>
+                                <TableCell align="center">Agency</TableCell>
+                                <TableCell align="center">Category</TableCell>
+                                <TableCell align="center">Volunteer Opportunities</TableCell>
+                                <TableCell align="center">Website Link</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                                <TableRow>
-                                    <TableCell align="right">test</TableCell>
-                                    <TableCell align="right">test</TableCell>
-                                    <TableCell align="right">test</TableCell>
-                                    <TableCell align="right"><Button className={this.props.classes.backButton}variant="contained">Volunteer</Button></TableCell>
-                                    <TableCell align="right"><Button className={this.props.classes.backButton} variant="contained">Website</Button></TableCell>
+                            <TableRow className={this.props.classes.rows} hover={true}>
+                                    <TableCell align="center">test</TableCell>
+                                    <TableCell align="center">test</TableCell>
+                                    <TableCell align="center">test</TableCell>
+                                    <TableCell align="center"><Button className={this.props.classes.backButton}variant="contained">Volunteer</Button></TableCell>
+                                    <TableCell align="center"><Button className={this.props.classes.backButton} variant="contained">Website</Button></TableCell>
                                 </TableRow>
                             <TableRow>
                                 <TableCell align="right">test</TableCell>
