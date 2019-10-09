cbfa954511b9991d31248eebaf6b310ad63386a6
wherewolfe35 <3wolfeaaron5@gmail.com>
2019-10-09 10:34:07 -0500

    adds styling

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 
 import './calendar.css';
 
@@ -23,57 +24,49 @@ const Calendar = () => {
     }
   }
 
+  function eventItem(event) {
+    let path = `/event/${event.id}`;
+    return (
+      <>
+        <Grid item md={12} key={event.id}> <b>Event:</b> {event.name} &nbsp;
+          <Link to={path}>Learn More</Link>
+        </Grid>
+        <Grid item md={12} key={event.id}><b> Organization: </b> {findName(event.non_profit_id)}
+          <br /> <b>Dates: </b>
+          {moment(event.start_date).format('MMM Do YYYY')} - {moment(event.end_date).format('MMM Do YYYY')}
+        </Grid> <br />
+      </>);
+  }
+
   return (
-    <div>
-      <div><h1>Calendar</h1></div>
-      <div>
-        <h4>{moment().subtract(7, 'days').calendar('MM[/]DD[/]YYYY')} - {moment().subtract(1, 'days').calendar('MM[/]DD[/]YYYY')}</h4>
-        <ul>
-          {eventList.lastWeek ? eventList.lastWeek.map(event =>
-            <li key={event.id}> <b>Event:</b> {event.name} &nbsp;
-            <Link to='/event'>Learn More</Link>
-              <b> Organization: </b> {findName(event.non_profit_id)}
-              <br /> <b>Dates: </b>
-              {moment(event.start_date).format('MMM Do YYYY')} - {moment(event.end_date).format('MMM Do YYYY')}
-            </li>) : 'No Events this week'}
-        </ul>
-      </div>
-      <div>
-        <h4>{moment().format('MM[/]DD[/]YYYY')} - {moment().add(6, 'days').calendar('MM[/]DD[/]YYYY')}</h4>
-        <ul>
-          {eventList.currentWeek ? eventList.currentWeek.map(event =>
-            <li key={event.id}> <b>Event:</b> {event.name} &nbsp;
-            <Link to='/event'>Learn More</Link>
-              <b> Organization: </b> {findName(event.non_profit_id)}
-              <br /> <b>Dates: </b>
-              {moment(event.start_date).format('MMM Do YYYY')} - {moment(event.end_date).format('MMM Do YYYY')}
-            </li>) : 'No Events this week'}
-        </ul>
-      </div>
-      <div>
-        <h4>{moment().add(7, 'days').calendar()} - {moment().add(13, 'days').calendar()}</h4>
-        <ul>
-          {eventList.nextWeek ? eventList.nextWeek.map(event =>
-            <li key={event.id}> <b>Event:</b> &nbsp; {event.name}
-              <Link to='/event'>Learn More</Link>
-              <b> Organization: </b> {findName(event.non_profit_id)}
-              <br /> <b>Dates: </b>
-              {moment(event.start_date).format('MMM Do YYYY')} - {moment(event.end_date).format('MMM Do YYYY')}
-            </li>) : 'No Events this week'}
-        </ul>
-      </div>
-      <div>
-        <h4>Future Events</h4>
-        <ul>
-          {eventList.nextWeek ? eventList.future.map(event =>
-            <li key={event.id}> <b>Event:</b> {event.name} &nbsp;
-            <Link to='/event'>Learn More</Link>
-              <b> Organization: </b> {findName(event.non_profit_id)}
-              <br /> <b>Dates: </b>
-              {moment(event.start_date).format('MMM Do YYYY')} - {moment(event.end_date).format('MMM Do YYYY')}
-            </li>) : 'No Events this week'}
-        </ul>
-      </div>
+    <div className="calendarPage">
+      <div className="calendarHeader"><h1>Calendar</h1></div>
+      <Grid container spacing={4}>
+        <Grid item md={6} xs={12}>
+          <h4 className="calendarDividers">{moment().subtract(7, 'days').calendar('MM[/]DD[/]YYYY')} - {moment().subtract(1, 'days').calendar('MM[/]DD[/]YYYY')}</h4>
+          <ul>
+            {eventList.lastWeek ? eventList.lastWeek.map(event => eventItem(event)) : 'No Events this week'}
+          </ul>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <h4 className="calendarDividers">{moment().format('MM[/]DD[/]YYYY')} - {moment().add(6, 'days').calendar('MM[/]DD[/]YYYY')}</h4>
+          <ul>
+            {eventList.currentWeek ? eventList.currentWeek.map(event => eventItem(event)) : 'No Events this week'}
+          </ul>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <h4 className="calendarDividers">{moment().add(7, 'days').calendar()} - {moment().add(13, 'days').calendar()}</h4>
+          <ul>
+            {eventList.nextWeek ? eventList.nextWeek.map(event => eventItem(event)) : 'No Events this week'}
+          </ul>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <h4 className="calendarDividers">Future Events</h4>
+          <ul>
+            {eventList.nextWeek ? eventList.future.map(event => eventItem(event)) : 'No Events this week'}
+          </ul>
+        </Grid>
+      </Grid>
     </div>
   );
 }
