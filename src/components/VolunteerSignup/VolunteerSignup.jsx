e8119a1b1b03b01fb43d75fd69b0c0ab7848ad83
wherewolfe35 <3wolfeaaron5@gmail.com>
2019-10-10 16:39:25 -0500

    merge with master

--- src/components/VolunteerSignup/VolunteerSignup.jsx
+++ src/components/VolunteerSignup/VolunteerSignup.jsx
@@ -63,9 +63,6 @@ class VolunteerSignup extends Component {
 
     render () {
 
-        let id = this.props.match.params.id
-
-
         return (
 
             <div className={this.props.classes.rootDiv} >
