6558f12ecaaf03010cd768b558e29355c4b59f63
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-10-11 11:10:56 -0500

    creating edit event details page

---
+++ src/components/EditEvent/EditEvent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditEvent extends Component {
+    render() {
+        return (
+            <div>
+                in edit event
+            </div>
+        )
+    }
+}
+
+export default EditEvent;
