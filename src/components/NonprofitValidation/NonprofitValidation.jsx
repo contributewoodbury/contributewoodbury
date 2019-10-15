69d69e2229b8039354a9a88140be761b5856cf5c
wherewolfe35 <3wolfeaaron5@gmail.com>
2019-10-15 15:14:34 -0500

    changes validation title and gets rid of stringify

--- src/components/NonprofitValidation/NonprofitValidation.jsx
+++ src/components/NonprofitValidation/NonprofitValidation.jsx
@@ -115,7 +115,7 @@ class NonprofitValidation extends Component {
 
 
             <div>
-                <h1>Nonprofit Validation Admin Page View</h1>
+                <h1>Nonprofit Validation</h1>
                 
                 <Button className={this.props.classes.directoryButton} onClick={this.handleDirectoryButton} variant="contained">Directory</Button>
                 
@@ -150,8 +150,6 @@ class NonprofitValidation extends Component {
                     ))}
                     
                 </Grid>
-
-                {JSON.stringify(this.props.reduxStore.admin.admin)}
             </div>
         )
     }
