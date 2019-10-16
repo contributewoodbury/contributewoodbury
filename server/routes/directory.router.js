7eaa79c895467ac460cbe6b87e159535d88a750e
wherewolfe35 <3wolfeaaron5@gmail.com>
2019-10-16 09:13:34 -0500

    corrects search route

--- server/routes/directory.router.js
+++ server/routes/directory.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // Gathers all nonprofits from the database who have been approved
 router.get('/', (req, res) => {
+  console.log(req.query.q);
   if (!req.query.q){
   let queryText = `SELECT * FROM "nonprofit" WHERE "is_approved" ORDER BY "name";`;
   pool.query(queryText)
@@ -17,7 +18,7 @@ router.get('/', (req, res) => {
 } else {
     let search = '%' + req.query.q + '%';
     let queryText = `SELECT * FROM "nonprofit" WHERE "name" ILIKE $1 OR "description" ILIKE $1 ORDER BY "name";`;
-    if(req.user.name === 'Admin') {
+    if(req.user && req.user.name === 'Admin') {
       queryText = `SELECT * FROM "nonprofit" WHERE "name" ILIKE $1 OR "description" ILIKE $1 ORDER BY "last_confirmed";`;
     }
     pool.query(queryText, [search])
